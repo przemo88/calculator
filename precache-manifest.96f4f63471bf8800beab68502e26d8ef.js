d96959f00aad5905c2fd91d7d6a83ae2fda4d8af
przemo88 <blagoff88@gmail.com>
2022-04-16 10:29:43 +0200

    Updates

---
+++ precache-manifest.96f4f63471bf8800beab68502e26d8ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99aede895a2157f8ff63291ccf56e3af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47beb08730b53e316e6b",
+    "url": "/static/css/main.98e44020.chunk.css"
+  },
+  {
+    "revision": "04d8920d5a3b44a43fc7",
+    "url": "/static/js/2.57f49728.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57f49728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47beb08730b53e316e6b",
+    "url": "/static/js/main.4337e3d2.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
